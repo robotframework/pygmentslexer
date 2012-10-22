8216c31363267a1110710781dcd119c4fa71134c
Pekka Kl?rck <Pekka Kl?rck@localhost>
2012-10-22 15:58:16 +0300

    cleaning up project structure

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-pygmentize -S default -f html > styles.css
-echo '<html><head>' > out.html
-echo '<link rel="stylesheet" type="text/css" href="styles.css" media="all">' >> out.html
-echo '</head><body>' >> out.html
-pygmentize -f html -l robotframework tests.txt >> out.html
-echo '</body></html>' >> out.html
+out=tmp/out.html
+styles=tmp/styles.css
+
+pygmentize -S default -f html > $styles
+echo '<html><head>' > $out
+echo '<link rel="stylesheet" type="text/css" href="styles.css">' >> $out
+echo '</head><body>' >> $out
+pygmentize -f html -l robotframework tests.txt >> $out
+echo '</body></html>' >> $out
