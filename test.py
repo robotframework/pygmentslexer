3fd9841aba649ca179ff84ffd96b568a09471f82
Pekka Kl?rck <Pekka Kl?rck@localhost>
2012-10-31 15:11:16 +0200

    python based test script. also keep result in version control to allow testing by diffing.

---
+++ test.py
@@ -0,0 +1,15 @@
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+
+from robotframeworklexer import RobotFrameworkLexer
+
+
+inpath = 'tests.txt'
+outpath = 'tests.html'
+
+with open(inpath) as infile:
+    with open(outpath, 'w') as outfile:
+        formatter = HtmlFormatter(full=True, style='autumn', encoding='UTF-8')
+        highlight(infile.read(), RobotFrameworkLexer(), formatter, outfile)
+
+print outpath
