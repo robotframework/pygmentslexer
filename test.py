4f25fca56c3ff442d4540fef5d2230bdcebcd759
Ilmari Kontulainen <kontulai@gmail.com>
2012-11-05 09:02:31 -0600

    ease style testing

--- test.py
+++ test.py
@@ -8,11 +8,12 @@ from robotframeworklexer import RobotFrameworkLexer
 
 
 inpath = 'tests.txt' if len(sys.argv) < 2 else sys.argv[1]
-outpath = 'out.html' if len(sys.argv) < 3 else sys.argv[2]
+outpath = 'out.html'
+style = 'autumn' if len(sys.argv) < 3 else sys.argv[2]
 
 with open(inpath) as infile:
     with open(outpath, 'w') as outfile:
-        formatter = HtmlFormatter(full=True, style='autumn', encoding='UTF-8')
+        formatter = HtmlFormatter(full=True, style=style, encoding='UTF-8')
         highlight(infile.read(), RobotFrameworkLexer(), formatter, outfile)
 
 print outpath
