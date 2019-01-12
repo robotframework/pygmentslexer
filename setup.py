ea1a73a7597dbe16e316ca93351ae25ebdaa7172
Pekka Klärck <peke@iki.fi>
2019-01-13 00:01:05 +0200

    Tuning packaging

--- setup.py
+++ setup.py
@@ -2,17 +2,34 @@
 
 """Pygments lexer for Robot Framework test data."""
 
+from os.path import abspath, dirname, join
+import re
+
 from setuptools import setup
 
-
-entry_points = '''
+NAME = 'robotframeworklexer'
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+'''.strip().splitlines()
+CURDIR = dirname(abspath(__file__))
+with open(join(CURDIR, NAME + '.py')) as source:
+    VERSION = re.search("\n__version__ = '(.*)'", source.read()).group(1)
+with open(join(CURDIR, 'README.rst')) as readme:
+    README = readme.read()
+with open(join(CURDIR, 'requirements.txt')) as requirements:
+    REQUIREMENTS = requirements.read().splitlines()
+ENTRY_POINTS = '''
 [pygments.lexers]
 robotframework = robotframeworklexer:RobotFrameworkLexer
 '''
 
 setup(
-    name         = 'robotframeworklexer',
-    version      = 'dev',
+    name         = NAME,
+    version      = VERSION,
     description  = __doc__,
     long_description = open('README.rst').read(),
     author       = u'Pekka Kl\xe4rck',
@@ -22,7 +39,7 @@ setup(
     download_url = 'https://pypi.org/project/robotframeworklexer',
     keywords     = 'pygments robotframework',
     platforms    = 'any',
-    py_modules   = ['robotframeworklexer'],
-    entry_points = entry_points,
-    requires     = ['pygments']
+    py_modules   = [NAME],
+    entry_points = ENTRY_POINTS,
+    requires     = REQUIREMENTS
 )
